5566701679ea9987a09f3d6cf6e6a4390e3e0a6e
vks1999 <vsr2399@gmail.com>
2022-09-12 15:51:45 +0530

    Practice

---
+++ Javascript/12Sept 2022-JS-5(Function)/one.js
@@ -0,0 +1,40 @@
+// // JS Function
+
+// function add() {
+//     console.log("Addition");
+// }
+
+// // Function with parameters
+
+// function add(a, b) {
+//     console.log(a + b);
+// }
+
+// add(10, 4); //14
+// add(5, 34); //39
+// add(4, 3); //7
+
+// Function with parameters & default value
+
+// function add(a, b, c = 3) {
+//     console.log(a + b + c);
+// }
+
+// add(10, 2, 15);
+// add(5, 4);
+
+// function with return
+
+// function add(a, b) {
+//     return a + b;
+// }
+// let r1 = add(10, 15);
+// console.log(r1);
+// let r5 = add(44, 62);
+// console.log(r5);
+
+function calAge(years) {
+    return 2022;
+}
+let age1 = calAge(1983);
+console.log(age1);
